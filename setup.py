4244cc7db4c7ffaafbe21ebd5aae779eed3d75e8
Saroj Rai <114452296+Saroj94@users.noreply.github.com>
2026-09-07 21:11:21 +0100

    First stage

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='src',
-    version='0.0.1',
+    version='0.1.0',
     author="Saroj",
     author_email="sarojrai.in@gmail.com",
     packages=find_packages()
