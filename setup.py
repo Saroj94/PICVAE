b7520710791fedc6243c777aef6f70904211bafc
Saroj Rai <114452296+Saroj94@users.noreply.github.com>
2026-09-07 01:20:58 +0100

    Folder structure is establish

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author="Saroj",
+    author_email="sarojrai.in@gmail.com",
+    packages=find_packages()
+)
